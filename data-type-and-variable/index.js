6efb58744757137fe29af8f42c58eaf325bdd4ba
rokib97 <hasanrokib1997@gmail.com>
2024-07-14 01:11:33 +0600

    first day done

---
+++ data-type-and-variable/index.js
@@ -0,0 +1,37 @@
+// declare variable
+var num1 = 97;
+// console.log(num1);
+let myName = "rokib";
+// console.log(myName);
+
+// constant declaration
+const isAdmin = true;
+// console.log(isAdmin);
+
+// data types
+let myFavNum = 7;
+let myLastName = "rokib";
+let isUser = false;
+const friends = ["saki", "sakib", "rokib"];
+const userOne = {
+  name: "rokib",
+  age: 24,
+  isMarried: false,
+};
+// console.log(typeof myFavNum);
+// console.log(typeof myLastName);
+// console.log(typeof isUser);
+// console.log(typeof friends);
+// console.log(typeof userOne);
+
+// reassign variables
+let myNumber = 69;
+myNumber = 96;
+console.log(`my initial number is ${myNumber}`);
+console.log(`my reassigned number is ${myNumber}`);
+
+// understanding const
+
+const userNum = 98;
+userNum = 89; // re-Assignment to constant variable is not possible.
+// console.log(userNum);
